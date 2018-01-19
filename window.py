7819cbbb8baf1b5e9ea51d995f52bb6a2e2b7fc3
Anton Yakovlev <lantar23@yandex.ru>
2018-01-19 23:34:33 +0300

    сохранение аудиопараметров, воспроизвдение

--- window.py
+++ window.py
@@ -18,22 +18,34 @@ class MyApp(QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
+        self.setAudioParams()
         self.btnRecord.clicked.connect(self.btnRecordClicked)
+        self.btnPlay.clicked.connect(self.btnPlayClicked)
+
+    def setAudioParams(self):
+        self.audioParams = {
+            'CHUNK' : 1000,
+            'FORMAT' : pyaudio.paInt16,
+            'CHANNELS' : 1,
+            'RATE' : 40000,
+            'WAVE_OUTPUT_FILENAME' : "output.wav"}
 
     def btnRecordClicked(self):
         seconds = self.boxDuration.value()
-        self.rawData = record(seconds)
+        self.rawData = record(seconds, self.audioParams)
         self.rawAmplitudeGraph = plot(self.rawData)
         self.lblRawSound.setPixmap(self.rawAmplitudeGraph)
-        print("debug")
 
-def record(duration):
-    CHUNK = 1000
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 40000
-    RECORD_SECONDS = duration
-    WAVE_OUTPUT_FILENAME = "output.wav"
+    def btnPlayClicked(self):
+        play(self.rawData, self.audioParams)
+
+def record(duration, audioParams):
+    chunk = audioParams['CHUNK']
+    format = audioParams['FORMAT']
+    channels = audioParams['CHANNELS']
+    rate = audioParams['RATE']
+    wave_out_filename = audioParams['WAVE_OUTPUT_FILENAME']
+    audioParams['DURATION'] = duration
 
     print("2...")
     time.sleep(1)
@@ -42,18 +54,18 @@ def record(duration):
 
     p = pyaudio.PyAudio()
 
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
+    stream = p.open(format=format,
+                    channels=channels,
+                    rate=rate,
                     input=True,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=chunk)
 
     print("Recording...")
 
     frames = []
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
+    for i in range(0, int(rate / chunk * duration)):
+        data = stream.read(chunk)
         frames.append(data)
 
     print("Done recording.")
@@ -66,17 +78,38 @@ def record(duration):
     for f in frames:
         b.append(np.frombuffer(f, dtype=np.int16))
 
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
+    wf = wave.open(wave_out_filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(format))
+    wf.setframerate(rate)
     wf.writeframes(b''.join(frames))
     wf.close()
 
     return np.ravel(b)
 
+def play(data, audioParams):
+    chunk = audioParams['CHUNK']
+    format = audioParams['FORMAT']
+    channels = audioParams['CHANNELS']
+    rate = audioParams['RATE']
+    duration = audioParams['DURATION']
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=format,
+                    channels=channels,
+                    rate=rate,
+                    output=True,
+                    frames_per_buffer=chunk)
+
+    stream.write(data, rate * duration)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
 def plot(data):
-    plt.figure(figsize=[5, 0.4])
+    plt.figure(figsize=[5, 0.6])
     # todo настроить оси
     plt.plot(data)
     plt.savefig('wave.png', fmt='png')
