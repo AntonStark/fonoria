0b38c1a705b155c59cae61476842fe300400200d
Anton Yakovlev <Lantar23@yandex.ru>
2018-01-26 23:02:56 +0300

    выделение самых интенсивных частот, правки стиля кода

--- window.py
+++ window.py
@@ -6,6 +6,7 @@ from PyQt5.QtGui import QImage, QPixmap
 
 import pyaudio, time, wave
 import numpy as np
+import numpy.ma as ma
 import matplotlib.pyplot as plt
 from datetime import datetime
 
@@ -13,33 +14,35 @@ qtCreatorFile = "./mainwindow.ui"  # Путь к UI файлу
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
+
 class MyApp(QMainWindow, Ui_MainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
-        self.setAudioParams()
-        self.btnRecord.clicked.connect(self.btnRecordClicked)
-        self.btnPlay.clicked.connect(self.btnPlayClicked)
-        self.boxDuration.valueChanged.connect(self.setAudioParams)
-        self.momentSelector.valueChanged.connect(self.refreshMomentumSpectrum)
-
-    def setAudioParams(self):
         self.audioParams = {
-            'CHUNK' : 1024,
-            'FORMAT' : pyaudio.paInt32,
-            'CHANNELS' : 1,
-            'RATE' : 8192,
-            'WAVE_OUTPUT_FILENAME' : "output.wav",
-            'DURATION' : self.boxDuration.value()}
+            'CHUNK': 1024,
+            'FORMAT': pyaudio.paInt32,
+            'CHANNELS': 1,
+            'RATE': 8192,
+            'WAVE_OUTPUT_FILENAME': "output.wav",
+            'DURATION': self.boxDuration.value()}
 
-    def printProgress(self, sec):
+        self.btnRecord.clicked.connect(self.btn_record_clicked)
+        self.btnPlay.clicked.connect(self.btn_play_clicked)
+        self.boxDuration.valueChanged.connect(self.reset_duration)
+        self.momentSelector.valueChanged.connect(self.refresh_momentum_spectrum)
+
+    def reset_duration(self, dur):
+        self.audioParams['DURATION'] = dur
+
+    def print_progress(self, sec):
         dur = self.audioParams['DURATION']
         self.btnRecord.setText('{}/{}'.format(sec, dur))
         self.repaint()
 
-    def btnRecordClicked(self):
+    def btn_record_clicked(self):
         self.btnRecord.setText('3..')
         self.repaint()
         time.sleep(0.5)
@@ -50,8 +53,8 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.repaint()
         time.sleep(0.5)
 
-        self.printProgress(0)
-        self.rawData = record(self.audioParams, self.printProgress)
+        self.print_progress(0)
+        self.rawData = record(self.audioParams, self.print_progress)
         self.btnRecord.setText('Запись')
         self.btnPlay.setEnabled(True)
         self.momentSelector.setEnabled(True)
@@ -61,20 +64,21 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.lblRawAmplitude.setPixmap(self.rawAmplitudeGraph)
 
         start = datetime.now()
-        self.spectrum = calcSpectrum(self.rawData[1], self.audioParams['RATE'])
+        self.spectrum = calc_spectrum(self.rawData[1], self.audioParams['RATE'])
         calc_time = datetime.now() - start
 
         self.lblRawSpectrum.setPixmap(self.spectrum[0])
         self.lblCalcTime.setText('FFT ~ {}мс.'
                                  .format(calc_time.microseconds / 1000))
 
-    def btnPlayClicked(self):
+    def btn_play_clicked(self):
         play(self.rawData[1], self.audioParams)
 
-    def refreshMomentumSpectrum(self, momentIndex):
-        self.lblMomentumSpectrum.setPixmap(getMomentumSpectrum(self.spectrum[1], momentIndex / 10000))
+    def refresh_momentum_spectrum(self, moment_index):
+        self.lblMomentumSpectrum.setPixmap(get_momentum_spectrum(self.spectrum[1], moment_index / 10000))
 
-def record(audioParams, printProgress):
+
+def record(audioParams, print_progress):
     chunk = audioParams['CHUNK']
     format = audioParams['FORMAT']
     channels = audioParams['CHANNELS']
@@ -92,19 +96,19 @@ def record(audioParams, printProgress):
     frames = []
 
     fps = rate / chunk
-    nFrames = int(fps * duration)
-    remainder = rate * duration - chunk * nFrames
+    n_frames = int(fps * duration)
+    remainder = rate * duration - chunk * n_frames
 
-    printProgress(0)
-    oldSed = 0
-    for i in range(0, nFrames):
+    print_progress(0)
+    old_sed = 0
+    for i in range(0, n_frames):
         data = stream.read(chunk)
         frames.append(data)
 
         sec = int(i / fps)
-        if sec != oldSed:
-            printProgress(sec)
-            oldSed = sec
+        if sec != old_sed:
+            print_progress(sec)
+            old_sed = sec
 
     data = stream.read(remainder)
     frames.append(data)
@@ -113,9 +117,9 @@ def record(audioParams, printProgress):
     stream.close()
     p.terminate()
 
-    I = np.array([], dtype=np.int32)
+    intensities = np.array([], dtype=np.int32)
     for f in frames:
-        I = np.append(I, np.frombuffer(f, dtype=np.int32))
+        intensities = np.append(intensities, np.frombuffer(f, dtype=np.int32))
 
     t = np.arange(0.0, duration, 1.0 / rate)
 
@@ -127,14 +131,14 @@ def record(audioParams, printProgress):
     # wf.writeframes(b''.join(frames))
     # wf.close()
 
+    return t, intensities
 
-    return t, I
 
-def play(data, audioParams):
-    chunk = audioParams['CHUNK']
-    format = audioParams['FORMAT']
-    channels = audioParams['CHANNELS']
-    rate = audioParams['RATE']
+def play(data, audio_params):
+    chunk = audio_params['CHUNK']
+    format = audio_params['FORMAT']
+    channels = audio_params['CHANNELS']
+    rate = audio_params['RATE']
 
     p = pyaudio.PyAudio()
 
@@ -149,6 +153,7 @@ def play(data, audioParams):
     stream.stop_stream()
     stream.close()
     p.terminate()
+
 
 def plot(data):
     t, I = data
@@ -166,7 +171,8 @@ def plot(data):
     im = QImage(buf, width, height, QImage.Format_RGB888)
     return QPixmap(im)
 
-def calcSpectrum(data, rate):
+
+def calc_spectrum(data, rate):
     # t = np.arange(0.0, 3.0, 2.**-13.)
     # s2 = 2 * np.sin(2 * np.pi * 200 * t)
     fig = plt.figure(figsize=[5, 2.5], frameon=False)
@@ -186,14 +192,40 @@ def calcSpectrum(data, rate):
     im = QImage(buf, width, height, QImage.Format_RGB888)
     return QPixmap(im), spectrum
 
-def getMomentumSpectrum(spectrum, partOfDuration):
-    nFrame = int(partOfDuration * spectrum.shape[1])
+
+def eval_fundamental(intensity):
+    freq_step = 16
+    fund_lower_bound = 50
+    fund_upper_bound = 300
+    n_main_comp = 20
+
+    indices = np.arange(intensity.size)
+    ind_round = int(fund_lower_bound / freq_step / 2)
+
+    important = []
+    intensity = ma.masked_where(indices < fund_lower_bound / freq_step, intensity)
+    for n in range(0, n_main_comp):
+        max = np.argmax(intensity)
+        important.append((max * freq_step, intensity.max()))
+        intensity = ma.masked_where(abs(indices - max) <= ind_round, intensity)
+
+    freq = np.array(important)[:, 0]
+    return freq
+
+
+def get_momentum_spectrum(spectrum, part_of_duration):
+    n_frames = int(part_of_duration * spectrum.shape[1])
 
     fig = plt.figure(figsize=[4, 4], frameon=False)
     ax = fig.add_subplot(111)
-    intensity = spectrum[:, nFrame]
+    intensity = spectrum[:, n_frames]
     ax.plot(np.linspace(0, 4096, intensity.size), intensity)
     ax.set_yscale('log', basey=10)
+    ax.grid()
+
+    freq_fund = eval_fundamental(intensity)
+    for x in freq_fund:
+        ax.axvline(x, color='green')
 
     canvas = fig.canvas
     canvas.draw()
@@ -201,6 +233,7 @@ def getMomentumSpectrum(spectrum, partOfDuration):
     (width, height) = canvas.get_width_height()
     im = QImage(buf, width, height, QImage.Format_RGB888)
     return QPixmap(im)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
